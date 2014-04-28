dff7c7bcfd2a8c22402443bbc9894fd89665f948
Juan Contreras <jc@thesilverlogic.com>
2014-04-28 18:38:58 -0400

    Initial commit with login screen working with the server

---
+++ PetTrainer/src/main/java/com/jj/pettrainer/gui/Global.java
@@ -0,0 +1,10 @@
+package com.jj.pettrainer.gui;
+
+/**
+ * Created by JCCP on 4/28/14.
+ */
+public class Global {
+
+    public static String API_LOGIN_URL = "http://pettrainer.fau.jh.gg/api/login/";
+
+}
